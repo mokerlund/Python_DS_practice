78fadb80e6b501636e31191ea4735f8c6c419ed3
Megan <okerlund.megan@gmail.com>
2019-06-10 21:49:07 -0700

    Bayes Theorem

---
+++ BayesTheorem.py
@@ -0,0 +1,5 @@
+#Bayes Theorem = P(A|B) =P(A)*P(B|A)/P(B)
+#Key insight = the probability of something that depends on B very much so
+#depends on the base probability of B and A
+
+#P(A|B) != P(B|A) statements aren't always true switched
